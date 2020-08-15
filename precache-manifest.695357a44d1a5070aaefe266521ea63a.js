68868676f8d5b39c73d1051b8a667f4c9f470773
Colin Turner <colinturner34@gmail.com>
2020-08-14 23:03:21 -0400

    Updates

---
+++ precache-manifest.695357a44d1a5070aaefe266521ea63a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addf1d1ad7f305cabe3c084b9820ef4c",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "c4d85610367bca5a1229",
+    "url": "/currency-converter/static/js/2.e9e8b602.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/currency-converter/static/js/2.e9e8b602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d5cb6696ede58eee70",
+    "url": "/currency-converter/static/js/main.1e1833ab.chunk.js"
+  },
+  {
+    "revision": "bad0ac1f66954a2504d5",
+    "url": "/currency-converter/static/js/runtime-main.2f40765a.js"
+  }
+]);
